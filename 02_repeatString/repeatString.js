ddb07679b2f05a7524ae6d7e81f95e681a1e0069
RobinFUnicus <robin.franzen@unicus.com>
2023-11-29 08:25:19 +0100

    Completed challenge 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numTimesRepeated) {
+    if(numTimesRepeated < 0){
+        return 'ERROR';
+    }
+    
+    let repeatedString = '';
+    
+    for(i=0; i<numTimesRepeated; i++){
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let charray = Array.from(phrase);
+    charray = charray.reverse();
+    let string = charray.join("");
+    return string
 };
 
 // Do not edit below this line
